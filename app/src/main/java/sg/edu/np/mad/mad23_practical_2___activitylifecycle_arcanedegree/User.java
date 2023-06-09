647c7a00b4cadc2e8a595b536c38dc75e30f1312
ArcaneDegree <s10239861@connect.np.edu.sg>
2023-06-09 12:19:56 +0800

    Changelog: - Created MainActivity class. - Created User class.

---
+++ app/src/main/java/sg/edu/np/mad/mad23_practical_2___activitylifecycle_arcanedegree/User.java
@@ -0,0 +1,11 @@
+package sg.edu.np.mad.mad23_practical_2___activitylifecycle_arcanedegree;
+
+public class User {
+    String name;
+
+    String description;
+
+    int id;
+
+    boolean followed;
+}
